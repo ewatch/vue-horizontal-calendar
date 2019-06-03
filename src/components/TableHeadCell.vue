dd1a5416bd1e1ba79aef6fef845b78b2ee726c4b
ewatch <ewatchhu@gmx.de>
2019-06-03 23:42:12 +0200

    initial commit, created TableHeadCell component and spec

---
+++ src/components/TableHeadCell.vue
@@ -0,0 +1,28 @@
+<template>
+    <th>
+        <span class="day-of-month">{{ dayOfMonth }}</span>
+        <span class="day-name">{{ dayName }}</span>
+        <span class="month-name">{{ monthName }}</span>
+    </th>
+</template>
+
+<script>
+export default {
+    props: {
+        dayOfMonth: {
+            type: Number,
+            required: true,
+            default: 0
+        },
+        dayName: {
+            type: String,
+            required: false
+        }, 
+        monthName: {
+            type: String,
+            required: false
+        }
+    }
+}
+</script>
+
