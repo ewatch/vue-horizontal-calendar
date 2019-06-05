554ec9add79eac35caf542c6084bbde36f0fcc74
ewatch <ewatchhu@gmx.de>
2019-06-06 00:47:29 +0200

    - removed getDayName (moved to helpers/date.js)

--- src/components/TableCalendar.vue
+++ src/components/TableCalendar.vue
@@ -52,10 +52,7 @@ export default {
     }
   },
   methods: {
-    getDayName: function(dayOfWeek) {
-      const dayNames = ["So", "Mo", "Di", "Mi", "Do", "Fr", "Sa"];
-      return dayNames[dayOfWeek];
-    }
+
   }
 };
 </script>
