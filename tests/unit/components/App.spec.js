e4f105524bab59242ab00fe66b6f12d75820f7b5
ewatch <ewatchhu@gmx.de>
2019-06-27 01:00:11 +0200

    - adds unit test for App component

---
+++ tests/unit/components/App.spec.js
@@ -0,0 +1,12 @@
+import { shallowMount } from "@vue/test-utils"
+import App from "@/App.vue"
+import TableCalendar from "@/components/TableCalendar.vue"
+
+describe('render App.vue', () => {
+    const wrapper = shallowMount(App)
+
+    it('the app contains the TableCalendar component', () => {
+        expect(wrapper.findAll(TableCalendar).length).toBe(1)
+    })
+
+})
